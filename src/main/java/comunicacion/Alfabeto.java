603a04ed81d6d8d91c165f75ce5d0c83ffd18d06
dtorresag <55595673+dtorresag@users.noreply.github.com>
2020-10-13 09:02:44 -0500

    d

---
+++ src/main/java/comunicacion/Alfabeto.java
@@ -0,0 +1,46 @@
+package comunicacion;
+
+import java.util.ArrayList;
+
+public class Alfabeto extends Pictograma {
+	
+	private String [] letras;
+	private String interpretacion;
+	
+	
+
+	public Alfabeto(String origen, String[] letras, String interpretacion) {
+		super(origen);
+		this.letras = letras;
+		this.interpretacion = interpretacion;
+	}
+
+	public String getInterpretacion() {
+		return interpretacion;
+	}
+
+	public void setInterpretacion(String interpretacion) {
+		this.interpretacion = interpretacion;
+	}
+		
+	public String interpretacion () {
+		return interpretacion;
+	}
+
+	@Override
+	public String toString() {
+		return "A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z";
+	}
+
+	public String[] getLetras() {
+		return letras;
+	}
+
+	public void setLetras(String[] letras) {
+		this.letras = letras;
+	}
+	
+	public int cantidadLetras() {
+		return letras.length;
+	}
+}
